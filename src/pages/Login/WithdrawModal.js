f02b4cd4e680b53b559ce15ff1a6af105b63d56f
CNiZ1945 <1945paly@gmail.com>
2024-08-14 09:58:06 +0900

    CNiZ 0814 10:00

---
+++ src/pages/Login/WithdrawModal.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+function WithdrawModal({ onClose }) {
+
+    const navigate = useNavigate();
+
+    return (
+        <>
+
+            <div className="modal_page">
+                <div className="modal_page_box">
+                    <div className="withdraw_announce">
+                        정말로 탈퇴하시겠습니까?
+                    </div>
+                    <br />
+                    <div>
+                        <button className="withdraw_yes" onClick={() => navigate("/")}>탈퇴하겠습니다</button>
+                    </div>
+                    <div className="withdraw_exit" onClick={onClose}>
+                        X
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+
+export default WithdrawModal
